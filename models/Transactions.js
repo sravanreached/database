38f72cd5541fa4dfe6e7408c351b30048cf435bb
sravan <sravan.s@reached.co.in>
2023-05-26 13:14:52 +0530

    transaction data table added

---
+++ models/Transactions.js
@@ -0,0 +1,59 @@
+// AdminModel.js
+var mongoose = require('mongoose');
+// Setup schema
+const transactionSchema = new mongoose.Schema({
+    RazorpayPaymentId: {
+      type: String,
+    },
+    RazorpayOrderId: {
+      type: String,
+      required: true,
+    },
+    Amount: {
+      type: Number,
+      required: true,
+    },
+    Currency: {
+      type: String,
+      required: true,
+    },
+    Method: {
+      type: String,
+    },
+    Email: {
+      type: String,
+    },
+    Contact:{
+      type: String,
+    },
+    RazorpayStatus: {
+      type: String,
+      required: true,
+    },
+    Createdby: {
+        type: String,
+    }, 
+    Modifiedby: {
+        type: String,
+    },
+},{
+    timestamps: {
+        createdAt: 'Created_date',
+        updatedAt: 'Modified_date',
+    },
+    toJSON: {
+        transform(doc, ret){
+            delete ret.__v;
+            delete ret.Created_date;
+            delete ret.Modified_date;
+            delete ret.Createdby;
+            delete ret.Modifiedby;
+        }
+    },
+});
+
+transactionSchema.index({
+    Createdby: 1,
+});
+
+module.exports = mongoose.model('Transaction', transactionSchema);
